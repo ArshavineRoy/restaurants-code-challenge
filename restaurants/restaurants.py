b081180a3b3b3480696238837195dfe0dfa5fc61
ArshavineRoy <arshavinewaema@gmail.com>
2023-08-25 15:49:02 +0300

    init Customer class and add given_name method - test passes

--- restaurants/restaurants.py
+++ restaurants/restaurants.py
@@ -5,10 +5,20 @@ class Restaurant:
     
 
 class Customer:
-    def __init__(self):
-    	pass
-    
+    def __init__(self, first_name, last_name):
+        if isinstance(first_name, str) or isinstance(last_name, str ):
+            self.first_name = first_name
+            self.last_name = last_name
+        else:
+            return "All names must be a string."
 
+    def given_name(self):
+        return self.first_name
+    
 class Review:
     def __init__(self):
         pass
+
+
+customer = Customer("John", "Doe")
+print(customer.given_name())
