bde8adf57c001a31c1aa8f0acd2a68ea8d03d83d
ArshavineRoy <arshavinewaema@gmail.com>
2023-08-25 15:41:00 +0300

    def basic classes in restaurants.py  - Restaurant  - Customer  - Review

---
+++ restaurants/restaurants.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+class Restaurant:
+    def __init__(self):
+        pass
+    
+
+class Customer:
+    def __init__(self):
+    	pass
+    
+
+class Review:
+    def __init__(self):
+        pass
